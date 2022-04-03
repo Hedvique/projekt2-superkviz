1a61bbf8793c995bc39dee4d742f26e420101eef
Simona Brabcová <simona.brabcova@email.cz>
2022-04-03 12:26:06 +0200

    vytvoření pole s otázkami

--- kviz.js
+++ kviz.js
@@ -1,1 +1,23 @@
-console.log(5)
+const otazky = [{
+    otazka: "Jaké je hlavní město Norska?",
+    obrazek: "obrazky/ovoce.jpg",
+    odpoved1: "Lillehamer",
+    odpoved2: "Oslo",
+    odpoved3: "Stockholm",
+    spravnaOdpoved: "Oslo"
+},{
+    otazka: "Jaké je hlavní město Islandu?",
+    obrazek: "obrazky/ovoce.jpg",
+    odpoved1: "Talin",
+    odpoved2: "Reykjavik",
+    odpoved3: "Trondheim",
+    spravnaOdpoved: "Reykjavik"
+}, {
+    otazka: "Jaké je hlavní město Finska?",
+    obrazek: "obrazky/ovoce.jpg",
+    odpoved1: "Lahti",
+    odpoved2: "Helsinky",
+    odpoved3: "Rovaniemi",
+    spravnaOdpoved: "Helsinky"
+}]
+
